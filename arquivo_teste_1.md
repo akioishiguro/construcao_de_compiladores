62ed3b1cba2f19cd5e1261ff6e88584b5931edda
akioishiguro <akiosantos@gmail.com>
2019-10-04 18:06:55 -0300

    Implementação de erros lexicos

--- arquivo_teste_1.md
+++ arquivo_teste_1.md
@@ -1,7 +1,25 @@
 #Comentario de linha
-#*Comentario de bloco
-*#
-
-    program nome_do_programa;
-corpo do texto
+#*    
+        Comentario de bloco! 
+        program nome_do_programa;
+            procedure nome_do_procedimento (paramentros_1:tipo ,parametro_2:tipo) 
+            begin
+                Procedimento
+            end;
+*# 
+program nome_do_programa;
+    procedure nome_do_procedimento (paramentros_1:char ,parametro_2:tipo) 
+    begin
+        Procedimento
+    end ;
+ fs >=
+    const nome_da_constante = tipo_constante; nome_da_constante = tipo_constante;
+    
+    declaravariaveis nove_variavel: tipo; nove_variavel: tipo; 
+    
+    begin
+        corpo 
+    end ;
 .
+
+
--- compilador.py
+++ compilador.py
@@ -1,3 +1,7 @@
+wordsDict = {"write": 0, "while": 1, "until": 2,  "to": 3,  "then": 4,  "string": 5,  "repeat": 6,  "real": 7,  "read": 8,  "program": 9,  "procedure": 10,  "or": 11,  "of": 12,  "literal": 13,  "integer": 14,  "if": 15,  "identificador": 16,  "î": 17,  "for": 18,  "end": 19,  "else": 20,  "do": 21,  "declaravariaveis": 22, "const": 23 ,"char": 24,  "chamaprocedure": 25,  "begin": 26,  "array": 27,  "and": 28,  "numreal": 36,  "numinteiro": 37, "nomestring": 38, "nomechar": 39}
+
+symbolsDict = { ">=": 29,  ">": 30,  "=": 31,  "<>": 32,  "<=": 33,  "<": 34,  "+": 35,  "]": 40,  "[": 41,  ";": 42,  ":": 43,  "/": 44,  "..": 45,  ".": 46,  ",": 47,  "*": 48,  ")": 49,  "(": 50,  "$": 51,  "-": 52 }
+
 def comentario(line_actual):
     for i in range(line_actual,num_lines):
         line=read_data_lines[i].strip()#Ignorando os espacos/tab/enter
@@ -10,36 +14,90 @@ def comentario(line_actual):
                 elif i == num_lines-1 :
                     return False
     
-def inicio():
-    print("program")
+def program(line_actual):
+    valid=0
+    for i in range(line_actual,num_lines):
+        line=read_data_lines[i].strip()#Ignorando os espacos/tab/enter
+        line_words=line.split(' ')##Dividimos as linhas em palavras
+        num_line_words=len(line_words)##NUmero de palavras na linha
+        for j in range(0,num_line_words):
+            if line_words[j].strip():
+                if i == line_actual and line_words[num_line_words-1].endswith(";"):
+                    valid+=1
+                    break
+                if line_words[j] == ".":
+                    valid+=1
+                elif i == num_lines-1 :
+                    return False
+                if valid == 2:
+                    return True
 
-def procedimento():
-    print("procedimento")
+def lexico_word(line_actual):
+    for i in range(line_actual,num_lines):
+        line=read_data_lines[i].strip()#Ignorando os espacos/tab/enter
+        line_words=line.split(' ')##Dividimos as linhas em palavras
+        num_line_words=len(line_words)##NUmero de palavras na linha
+        for j in range(0,num_line_words):
+            if line_words[j].strip():
+                if line_words[j] in wordsDict:
+                    print ("Palavra:",line_words[j],"| Token:",wordsDict[line_words[j]],"| Na linha:",i+1)
+                    
+                    ###Identificador
+                    if j!= num_line_words-1:
+                        next_word= line_words[j+1]
+                        actual_word= line_words[j]
 
-def constante():
-    print("constante")
+                        if actual_word in wordsDict :
+                            if next_word not in wordsDict:
+                                if next_word not in symbolsDict:
+                                    next_word=next_word.strip(":>=<+][;/..,*)($-")
+                                    print ("Palavra:",next_word,"| Token:",wordsDict["identificador"],"| Na linha:",i+1)
+                    
+    return True
 
-def variavel():
-    print("varialvel")
+def lexico_symbol(line_actual):
+    for i in range(line_actual,num_lines):
+        line=read_data_lines[i].strip()#Ignorando os espacos/tab/enter
+        line_words=line.split(' ')##Dividimos as linhas em palavras
+        num_line_words=len(line_words)##NUmero de palavras na linha
+        for j in range(0,num_line_words):
+            if line_words[j].strip():
+                aux_symbol=list(line_words[j])
+                aux_symbol_size=len(aux_symbol)
+                for k in range (0, aux_symbol_size):
+                    if aux_symbol[k] in symbolsDict:
+                        ###Simbolo Concatenado
+                        if k!= aux_symbol_size-1:
+                            next_symbol= aux_symbol[k+1]
+                            actual_symbol= aux_symbol[k]
 
-def corpo():
-    print("corpo")
+                            if actual_symbol in symbolsDict :
+                                if next_symbol in symbolsDict:
+                                    comp_symbol=actual_symbol+next_symbol
+                                    if comp_symbol in symbolsDict:
+                                        print ("Simbolo:",comp_symbol,"Token:",symbolsDict[comp_symbol],"| Na linha:",i+1)
+                                        break
+                        
+                        print ("Simbolo:",aux_symbol[k],"Token:",symbolsDict[aux_symbol[k]],"| Na linha:",i+1)
+    return True
+
 
 with open ('arquivo_teste_1.md') as arq:
-        read_data = arq.read()##read_data é uma string
+        read_data = arq.read()###read_data é o meu programa dentro de uma string
 
 
-read_data_lines=read_data.split('\n')
+read_data_lines=read_data.split('\n')###read_data_lines, separa todo o meu programa por linhas
 num_lines=len(read_data_lines)
 num_words=len(read_data)##NUmero total de palavras no arquivo
 aux_end_com_bloco=0
-jump=False
-
+check_word=False
+check_symbol=False
 for i in range(0, num_lines):
     line=read_data_lines[i].strip()#Ignorando os espacos/tab/enter
     line_words=line.split(' ')##Dividimos as linhas em palavras
     num_line_words=len(line_words)##NUmero de palavras na linha
-    if i>=aux_end_com_bloco:
+    line_actual=0
+    if i>aux_end_com_bloco:
         for j in range(0, num_line_words):
             if line_words[j].strip():
                 if line_words[j].startswith('#'):
@@ -56,13 +114,19 @@ for i in range(0, num_lines):
                     else:
                         print("temos um comentario na linha:",i+1)
                         break
-                elif line_words[j] == "program" :
-                    inicio()
-                elif line_words[j] == "procedure":
-                    procedimento()
-                elif line_words[j] == "const":
-                    constante()
-                elif line_words[j] == "declaravariaveis":
-                    variavel()
-                elif line_words[j] == "begin":
-                    corpo()
+                #elif line_words[j] == "program" :
+                #    line_actual=i
+                #    aux_begin=program(line_actual)
+                #    if aux_begin == True:
+                #        print("ok")
+                #        break
+                elif check_word==False:
+                    line_actual=i
+                    check_word=lexico_word(line_actual)
+
+                elif check_symbol==False:
+                    line_actual=i
+                    check_symbol=lexico_symbol(line_actual)
+                #
+                #else:
+                #    print("Erro na linha",i+1,"A palavra:",line_words[j],"Não existe na gramatica")
