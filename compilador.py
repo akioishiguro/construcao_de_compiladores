a594e0f9166883d88bbb2d551d459934003bdec8
akioishiguro <akiosantos@gmail.com>
2019-10-03 21:17:36 -0300

    Pula os comentarios por bloco

--- compilador.py
+++ compilador.py
@@ -1,5 +1,5 @@
-def comentario(line_atual):
-    for i in range(line_atual,num_lines):
+def comentario(line_actual):
+    for i in range(line_actual,num_lines):
         line=read_data_lines[i].strip()#Ignorando os espacos/tab/enter
         line_words=line.split(' ')##Dividimos as linhas em palavras
         num_line_words=len(line_words)##NUmero de palavras na linha
@@ -33,33 +33,36 @@ read_data_lines=read_data.split('\n')
 num_lines=len(read_data_lines)
 num_words=len(read_data)##NUmero total de palavras no arquivo
 aux_end_com_bloco=0
+jump=False
+
 for i in range(0, num_lines):
     line=read_data_lines[i].strip()#Ignorando os espacos/tab/enter
     line_words=line.split(' ')##Dividimos as linhas em palavras
     num_line_words=len(line_words)##NUmero de palavras na linha
-    for j in range(0, num_line_words):
-        if line_words[j].strip():
-            if line_words[j].startswith('#'):
-                if line_words[j].startswith('#*'):
-                    line_atual=i
-                    end_com_bloco=comentario(line_atual)
-                    if end_com_bloco != False and aux_end_com_bloco != end_com_bloco:
-                        aux_end_com_bloco=end_com_bloco
-                        print('Inicio do comentario de bloco na linha:', line_atual+1,' Final do Comentario de bloco na linha:',end_com_bloco+1)
-                        break
+    if i>=aux_end_com_bloco:
+        for j in range(0, num_line_words):
+            if line_words[j].strip():
+                if line_words[j].startswith('#'):
+                    if line_words[j].startswith('#*'):
+                        line_actual=i
+                        end_com_bloco=comentario(line_actual)
+                        if end_com_bloco != False and aux_end_com_bloco != end_com_bloco:
+                            aux_end_com_bloco=end_com_bloco
+                            print('Inicio do comentario de bloco na linha:', line_actual+1,' Final do Comentario de bloco na linha:',end_com_bloco+1)
+                            break
+                        else:
+                            print("ERRO LEXICO")
+                            break
                     else:
-                        print("ERRO LEXICO")
+                        print("temos um comentario na linha:",i+1)
                         break
-                else:
-                    print("temos um comentario na linha:",i)
-                    break
-            elif line_words[j] == "program" :
-                inicio()
-            elif line_words[j] == "procedure":
-                procedimento()
-            elif line_words[j] == "const":
-                constante()
-            elif line_words[j] == "declaravariaveis":
-                variavel()
-            elif line_words[j] == "begin":
-                corpo()
+                elif line_words[j] == "program" :
+                    inicio()
+                elif line_words[j] == "procedure":
+                    procedimento()
+                elif line_words[j] == "const":
+                    constante()
+                elif line_words[j] == "declaravariaveis":
+                    variavel()
+                elif line_words[j] == "begin":
+                    corpo()
